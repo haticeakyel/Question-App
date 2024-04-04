c45e7c749901abab9ca3197b5568290adfbe147e
Hatice Akyel <haticeeakyel@gmail.com>
2024-04-04 16:43:06 +0300

    add delete and edit apis of post

--- src/main/java/com/example/questapp/controllers/PostController.java
+++ src/main/java/com/example/questapp/controllers/PostController.java
@@ -1,6 +1,8 @@
 package com.example.questapp.controllers;
 
 import com.example.questapp.entities.Post;
+import com.example.questapp.requests.PostCreateRequest;
+import com.example.questapp.requests.PostUpdateRequest;
 import com.example.questapp.services.PostService;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,24 +12,38 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/posts")
 public class PostController {
-    private PostService postService;
+    private final PostService postService;
 
     public PostController(PostService postService) {
+
         this.postService = postService;
     }
 
     @GetMapping
     public List<Post> getAllPosts(@RequestParam Optional<Long> userId){
+
         return postService.getAllPosts(userId);
     }
 
     @GetMapping("/{postId}")
     public Post getOnePost(@PathVariable Long postId){
+
         return postService.getOnePost(postId);
     }
 
     @PostMapping
-    public Post CreatePost(@RequestBody Post newPost){
-        return postService.savePost(newPost);
+    public Post CreatePost(@RequestBody PostCreateRequest newPostRequest){
+        return postService.savePost(newPostRequest);
     }
+
+    @PutMapping("/{postId}")
+    public Post updatePost(@PathVariable Long postId, @RequestBody PostUpdateRequest postUpdateRequest){
+        return postService.updatePost(postId, postUpdateRequest);
+    }
+
+    @DeleteMapping("/{postId}")
+    public void deletePost(@PathVariable Long postId){
+        postService.deletePost(postId);
+    }
+
 }
--- src/main/java/com/example/questapp/services/PostService.java
+++ src/main/java/com/example/questapp/services/PostService.java
@@ -1,18 +1,22 @@
 package com.example.questapp.services;
 
 import com.example.questapp.entities.Post;
+import com.example.questapp.entities.User;
 import com.example.questapp.repos.PostRepository;
+import com.example.questapp.requests.PostCreateRequest;
+import com.example.questapp.requests.PostUpdateRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class PostService {
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
+    private final UserService userService;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, UserService userService) {
+        this.userService = userService;
         this.postRepository = postRepository;
     }
 
@@ -26,7 +30,31 @@ public class PostService {
         return postRepository.findById(postId).orElse(null);
     }
 
-    public Post savePost(Post newPost) {
-        return postRepository.save(newPost);
+    public Post savePost(PostCreateRequest newPostRequest) {
+        User user = userService.getOneUser(newPostRequest.getUserId());
+        if (user == null)
+            return null;
+        Post toSave = new Post();
+        toSave.setId(newPostRequest.getId());
+        toSave.setText(newPostRequest.getText());
+        toSave.setTitle(newPostRequest.getTitle());
+        toSave.setUser(user);
+        return postRepository.save(toSave);
     }
+
+    public Post updatePost(Long postId, PostUpdateRequest postUpdateRequest) {
+        Optional <Post> post = postRepository.findById(postId);
+        if (post.isPresent()){
+            Post toUpdate = post.get();
+            toUpdate.setText(postUpdateRequest.getText());
+            toUpdate.setTitle(postUpdateRequest.getTitle());
+            postRepository.save(toUpdate);
+            return toUpdate;
+        }
+        return null;
+    }
+    public void deletePost(Long postId) {
+         postRepository.deleteById(postId);
+    }
+
 }
